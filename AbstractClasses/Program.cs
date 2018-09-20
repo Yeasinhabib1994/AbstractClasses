cafe368ce79f7e3c9cb6d8525c2c2fd70b594664
yeasin habib <yeasinhabib1994@gmail.com>
2018-09-20 18:21:30 +0600

    Add project files.

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle = new Circle();
+            circle.Draw();
+
+            var rectangle = new Rectangle();
+            rectangle.Draw();
+        }
+    }
+
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public abstract void Draw();
+
+        public void Copy()
+        {
+            Console.WriteLine("copy shape into clipboard");
+        }
+
+        public void Select()
+        {
+            Console.WriteLine("select the shape");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a circle");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("draw a rectangle");
+        }
+    }
+}
